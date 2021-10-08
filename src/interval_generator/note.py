9c48deb1540764bebd92e6c32030d8ae19ab278b
Sathors <sathors@free.fr>
2021-10-08 14:56:58 -0500

    Intervals (positive and negative) are now working.

--- src/interval_generator/note.py
+++ src/interval_generator/note.py
@@ -1,10 +1,25 @@
 from operator import neg, pos
-from re import match
+from re import match, search
 
 NUMBER_OF_DIATONIC_NOTES = 7
 NUMBER_OF_CHROMATIC_NOTES = 12
 DIATONIC_NOTES = ["c", "d", "e", "f", "g", "a", "b"]
 DIATONIC_TO_CHROMATIC_NUMBER = {1: 0, 2: 2, 3: 4, 4: 5, 5: 7, 6: 9, 7: 11}
+INTERVALS = {
+    0: "P1 d2",
+    1: "m2 A1",
+    2: "M2 d3",
+    3: "m3 A2",
+    4: "M3 d4",
+    5: "P4 A3",
+    6: "d5 A4",
+    7: "P5 d6",
+    8: "m6 A5",
+    9: "M6 d7",
+    10: "m7 A6",
+    11: "M7 d8",
+    12: "P8 A7",
+}
 
 
 def is_flat(note):
@@ -15,27 +30,26 @@ def is_sharp(note):
     return bool(match(r"^.s$", note))
 
 
-def diatonic_distance_from_c4(note):
+def interval_number_from_c4(note):
     diatonic_height_from_octaves = note.count("'") * len(DIATONIC_NOTES)
     return 1 + DIATONIC_NOTES.index(note[0]) + diatonic_height_from_octaves
 
 
-def diatonic_distance_between(first_note, second_note):
+def interval_number_between(first_note, second_note):
     sign = (
         pos
-        if diatonic_distance_from_c4(first_note)
-        <= diatonic_distance_from_c4(second_note)
+        if interval_number_from_c4(first_note) <= interval_number_from_c4(second_note)
         else neg
     )
     return (
         sign(1)
-        + diatonic_distance_from_c4(second_note)
-        - diatonic_distance_from_c4(first_note)
+        + interval_number_from_c4(second_note)
+        - interval_number_from_c4(first_note)
     )
 
 
 def harmonic_distance_from_c4(note):
-    diatonic_distance = diatonic_distance_from_c4(note)
+    diatonic_distance = interval_number_from_c4(note)
     harmonic_distance = DIATONIC_TO_CHROMATIC_NUMBER[
         diatonic_distance % NUMBER_OF_DIATONIC_NOTES
     ]
@@ -50,3 +64,24 @@ def harmonic_distance_from_c4(note):
     elif is_flat(note):
         harmonic_distance -= 1
     return harmonic_distance
+
+
+def harmonic_distance_between(first_note, second_note):
+    return harmonic_distance_from_c4(second_note) - harmonic_distance_from_c4(
+        first_note
+    )
+
+
+def interval_between(note1, note2):
+    harmonic_distance = harmonic_distance_between(note1, note2)
+    interval_number = interval_number_between(note1, note2)
+    is_descending_interval = harmonic_distance < 0
+    if is_descending_interval:
+        harmonic_distance = neg(harmonic_distance)
+        interval_number = neg(interval_number)
+    possible_intervals = INTERVALS[harmonic_distance % NUMBER_OF_CHROMATIC_NOTES]
+    quality = search(
+        f"(.{str(interval_number % NUMBER_OF_DIATONIC_NOTES)})",
+        possible_intervals,
+    )[0][0]
+    return ("-" if is_descending_interval else "") + quality + str(interval_number)
--- tests/test_interval_generator.py
+++ tests/test_interval_generator.py
@@ -2,9 +2,11 @@ from pytest import mark
 from src.interval_generator.note import (
     is_flat,
     is_sharp,
-    diatonic_distance_from_c4,
-    diatonic_distance_between,
+    interval_number_from_c4,
+    interval_number_between,
     harmonic_distance_from_c4,
+    harmonic_distance_between,
+    interval_between,
 )
 
 
@@ -41,8 +43,8 @@ def test_is_sharp(test_note, expected):
         ("c''", 15),
     ],
 )
-def test_diatonic_distance_from_c4(test_note, expected):
-    assert diatonic_distance_from_c4(test_note) == expected
+def test_interval_number_from_c4(test_note, expected):
+    assert interval_number_from_c4(test_note) == expected
 
 
 @mark.parametrize(
@@ -58,8 +60,8 @@ def test_diatonic_distance_from_c4(test_note, expected):
         ("d", "c", -2),
     ],
 )
-def test_diatonic_distance_between(test_note1, test_note2, expected):
-    assert diatonic_distance_between(test_note1, test_note2) == expected
+def test_interval_number_between(test_note1, test_note2, expected):
+    assert interval_number_between(test_note1, test_note2) == expected
 
 
 @mark.parametrize(
@@ -78,3 +80,37 @@ def test_diatonic_distance_between(test_note1, test_note2, expected):
 )
 def test_harmonic_distance_from_c4(test_note, expected):
     assert harmonic_distance_from_c4(test_note) == expected
+
+
+@mark.parametrize(
+    "test_note1,test_note2,expected",
+    [
+        ("c", "c", 0),
+        ("c", "d", 2),
+        ("d", "d'", 12),
+        ("d'", "d", -12),
+        ("e", "f", 1),
+        ("d", "c", -2),
+    ],
+)
+def test_harmonic_distance_between(test_note1, test_note2, expected):
+    assert harmonic_distance_between(test_note1, test_note2) == expected
+
+
+@mark.parametrize(
+    "test_note1,test_note2,expected",
+    [
+        ("c", "c", "P1"),
+        ("c", "d", "M2"),
+        ("c", "df", "m2"),
+        ("cs", "d", "m2"),
+        ("c'", "c''", "P8"),
+        ("c'", "c''", "P8"),
+        ("c", "d'", "M9"),
+        ("cf", "d'", "A9"),
+        ("d'", "cf", "-A9"),
+        ("d", "c", "-M2"),
+    ],
+)
+def test_interval_between(test_note1, test_note2, expected):
+    assert interval_between(test_note1, test_note2) == expected
