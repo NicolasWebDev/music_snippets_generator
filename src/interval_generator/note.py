c030ca1397f4bf3ab51eb17fcf840e012f578523
Sathors <sathors@free.fr>
2021-10-07 23:43:05 -0500

    Add a function to calculate the interval number from C4.

--- src/interval_generator/note.py
+++ src/interval_generator/note.py
@@ -1,5 +1,7 @@
 from re import match
 
+DIATONIC_NOTES = "c d e f g a b".split()
+
 
 def is_flat(note):
     return bool(match(r"^.b$", note))
@@ -7,3 +9,8 @@ def is_flat(note):
 
 def is_sharp(note):
     return bool(match(r"^.s$", note))
+
+
+def interval_number_from_c4(note):
+    diatonic_height_from_octaves = note.count("'") * len(DIATONIC_NOTES)
+    return 1 + DIATONIC_NOTES.index(note[0]) + diatonic_height_from_octaves
--- tests/test_interval_generator.py
+++ tests/test_interval_generator.py
@@ -1,8 +1,8 @@
-import pytest
-from src.interval_generator.note import is_flat, is_sharp
+from pytest import mark
+from src.interval_generator.note import is_flat, is_sharp, interval_number_from_c4
 
 
-@pytest.mark.parametrize(
+@mark.parametrize(
     "test_input,expected",
     [("ab", True), ("a", False), ("b", False), ("bb", True), ("bs", False)],
 )
@@ -10,9 +10,30 @@ def test_is_flat(test_input, expected):
     assert is_flat(test_input) == expected
 
 
-@pytest.mark.parametrize(
+@mark.parametrize(
     "test_input,expected",
     [("ab", False), ("a", False), ("b", False), ("bb", False), ("bs", True)],
 )
 def test_is_sharp(test_input, expected):
     assert is_sharp(test_input) == expected
+
+
+@mark.parametrize(
+    "test_input,expected",
+    [
+        ("c", 1),
+        ("d", 2),
+        ("ef", 3),
+        ("fs", 4),
+        ("g", 5),
+        ("ab", 6),
+        ("bs", 7),
+        ("c'", 8),
+        ("d'", 9),
+        ("fb'", 11),
+        ("as'", 13),
+        ("c''", 15),
+    ],
+)
+def test_interval_number_from_c4(test_input, expected):
+    assert interval_number_from_c4(test_input) == expected
