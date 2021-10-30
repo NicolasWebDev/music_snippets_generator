c1c7854bb2e70eebbc3d74cc29e1fdcce6ca018b
Sathors <sathors@free.fr>
2021-10-30 10:15:56 -0500

    Add tests.

--- tests/test_music_snippets_generator.py
+++ tests/test_music_snippets_generator.py
@@ -1,10 +1,26 @@
 from shutil import copy
+from unittest.mock import patch
 from filecmp import cmp
+from click.testing import CliRunner
 from src.music_snippets_generator.music_snippets_generator import (
     _generate_ascending_dyads_on_2_octaves,
     _pdf2svg,
     _ly2pdf,
+    cli,
 )
+
+
+def test_cli():
+    assert CliRunner().invoke(cli).exit_code == 0
+
+
+@patch(
+    "src.music_snippets_generator.music_snippets_generator."
+    "generate_anki_cards_with_images_of_dyads",
+)
+def test_dyads(mock_function):
+    assert CliRunner().invoke(cli, ["dyads"]).exit_code == 0
+    mock_function.assert_called_with()
 
 
 def test_ly2pdf(tmp_path):
